8eaca988bbf39bc4f586d4df679fafafaaceb235
Gauravsunil <32197840+Gauravsunil@users.noreply.github.com>
2020-05-29 09:30:59 +0530

    Edit form where we can edit details

--- EditContact.php
+++ EditContact.php
@@ -18,7 +18,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
     <title>Edit Contact</title>
 </head>
 <?php
-    include 'common.php';
+    include './scripts/common.php';
     $id=$_GET["id"];
     $query="select * from users where id='$id'";
     $result=mysqli_query($con,$query)or die(mysqli_error($con));
